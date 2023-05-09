4c39d56be6d1ad374a30c5fc2aa4e2e2240601c6
Agata Sularz <sularzagata@gmail.com>
2023-05-09 22:49:02 +0200

    Adjusting comments when, given, then

--- src/test/java/scoreboard/ScoreboardTest.java
+++ src/test/java/scoreboard/ScoreboardTest.java
@@ -35,134 +35,126 @@ class ScoreboardTest {
     }
 
     @Test
-    void whenCreateNewGame_givenTwoMatchesWithTheSameHomeTeam_thenAddNewPairToScoreboard() {
-        //when
-        Scoreboard scoreboard = new Scoreboard();
-
+    void whenCreateNewGame_givenSecondMatchWithTheSameHomeTeam_thenAddNewPairToScoreboard() {
         //given
+        Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(TeamAlreadyInMatchException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME_2);
         });
     }
 
     @Test
-    void whenCreateNewGame_givenTwoMatchesWithTheSameAwayTeam_thenAddNewPairToScoreboard() {
-        //when
-        Scoreboard scoreboard = new Scoreboard();
-
+    void whenCreateNewGame_givenSecondMatchWithTheSameAwayTeam_thenAddNewPairToScoreboard() {
         //given
+        Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(TeamAlreadyInMatchException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame(HOME_TEAM_NAME_2, AWAY_TEAM_NAME);
         });
     }
 
     @Test
     void whenCreateNewGame_givenNotUniquePair_thenThrowAnError() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(MatchAlreadyStartedException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
         });
     }
 
     @Test
     void whenCreateNewGame_givenNullHomeTeam_thenThrowAnError() {
-        //when
         Scoreboard scoreboard = new Scoreboard();
 
         //then
         assertThrows(IllegalArgumentException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame(null, AWAY_TEAM_NAME);
         });
     }
 
     @Test
     void whenCreateNewGame_givenNullAwayTeam_thenThrowAnError() {
-        //when
         Scoreboard scoreboard = new Scoreboard();
 
         //then
         assertThrows(IllegalArgumentException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame(HOME_TEAM_NAME, null);
         });
     }
 
     @Test
     void whenCreateNewGame_givenEmptyHomeTeam_thenThrowAnError() {
-        //when
         Scoreboard scoreboard = new Scoreboard();
 
         //then
         assertThrows(IllegalArgumentException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame("", AWAY_TEAM_NAME);
         });
     }
 
     @Test
     void whenCreateNewGame_givenEmptyAwayTeam_thenThrowAnError() {
-        //when
         Scoreboard scoreboard = new Scoreboard();
 
         //then
         assertThrows(IllegalArgumentException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame(HOME_TEAM_NAME, "");
         });
     }
 
     @Test
     void whenCreateNewGame_givenTheSameTeams_thenThrowAnError() {
-        //when
         Scoreboard scoreboard = new Scoreboard();
 
         //then
         assertThrows(NotUniquePairException.class, () -> {
-            //given
+            //when
             scoreboard.startNewGame(HOME_TEAM_NAME, HOME_TEAM_NAME);
         });
     }
 
     @Test
     void whenUpdateGame_givenExistingMatch_thenUpdateTheGame() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
         scoreboard.startNewGame(HOME_TEAM_NAME_2, AWAY_TEAM_NAME_2);
 
-        //given
+        //when
         scoreboard.updateGame(HOME_TEAM_NAME, AWAY_TEAM_NAME, 1, 2);
+
         //then
         var expected = new HashMap<String, Match>();
         expected.put(HOME_TEAM_NAME, new Match(HOME_TEAM_NAME, AWAY_TEAM_NAME, 1, 2));
         expected.put(HOME_TEAM_NAME_2, new Match(HOME_TEAM_NAME_2, AWAY_TEAM_NAME_2, 0, 0));
-
 
         assertEquals(expected, scoreboard.getScores());
     }
 
     @Test
     void whenUpdateGame_givenExistingMatchWithZeroValue_thenUpdateTheGame() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
-        //given
+        //when
         scoreboard.updateGame(HOME_TEAM_NAME, AWAY_TEAM_NAME, 1, 0);
+
         //then
         var expected = new HashMap<String, Match>();
         expected.put(HOME_TEAM_NAME, new Match(HOME_TEAM_NAME, AWAY_TEAM_NAME, 1, 0));
@@ -172,78 +164,78 @@ class ScoreboardTest {
 
     @Test
     void whenUpdateGame_givenWithNotExistingHomeTeamMatch_thenThrowException() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(MatchDoesntExistException.class, () -> {
-            //given
+            //when
             scoreboard.updateGame(HOME_TEAM_NAME_2, AWAY_TEAM_NAME, 1, 2);
         });
     }
 
     @Test
     void whenUpdateGame_givenWithNotExistingAwayTeamMatch_thenThrowException() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(MatchDoesntExistException.class, () -> {
-            //given
+            //when
             scoreboard.updateGame(HOME_TEAM_NAME, AWAY_TEAM_NAME_2, 1, 2);
         });
     }
 
     @Test
     void whenUpdateGame_givenNotExistingMatchWithNullHomeTeam_thenThrowException() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(MatchDoesntExistException.class, () -> {
-            //given
+            //when
             scoreboard.updateGame(null, AWAY_TEAM_NAME, 1, 2);
         });
     }
 
     @Test
     void whenUpdateGame_givenNotExistingMatchWithNullAwayTeam_thenThrowException() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(MatchDoesntExistException.class, () -> {
-            //given
+            //when
             scoreboard.updateGame(HOME_TEAM_NAME, null, 1, 2);
         });
     }
 
     @Test
     void whenUpdateGame_givenExistingMatchWithNegativeNumberOnHomeTeamScore_thenThrowException() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(IllegalArgumentException.class, () -> {
-            //given
+            //when
             scoreboard.updateGame(HOME_TEAM_NAME, AWAY_TEAM_NAME, -1, 2);
         });
     }
 
     @Test
     void whenUpdateGame_givenExistingMatchWithNegativeNumberOnAwayTeamScore_thenThrowException() {
-        //when
+        //given
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startNewGame(HOME_TEAM_NAME, AWAY_TEAM_NAME);
 
         //then
         assertThrows(IllegalArgumentException.class, () -> {
-            //given
+            //when
             scoreboard.updateGame(HOME_TEAM_NAME, AWAY_TEAM_NAME, 1, -2);
         });
     }
