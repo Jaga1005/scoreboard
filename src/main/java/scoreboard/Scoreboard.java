f94ed4d24d839f55fea9eb97d52f5cc328ee6a69
Agata Sularz <sularzagata@gmail.com>
2023-05-09 20:05:24 +0200

    Implement handling starting match for the team which has already started a match

--- src/main/java/scoreboard/Scoreboard.java
+++ src/main/java/scoreboard/Scoreboard.java
@@ -2,6 +2,7 @@ package scoreboard;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import scoreboard.exceptions.TeamAlreadyInMatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,9 +18,21 @@ public class Scoreboard {
     public void startNewGame(String homeTeam, String awayTeam) {
         log.info("Starting new game for homeTeam: {} and awayTeam: {}", homeTeam, awayTeam);
 
+        if (checkIfTeamHasStartedMatch(homeTeam)) {
+            log.error("Team {} has already started a match!", homeTeam);
+            throw new TeamAlreadyInMatchException();
+        } else if ( checkIfTeamHasStartedMatch(awayTeam)) {
+            log.error("Team {} has already started a match!", awayTeam);
+            throw new TeamAlreadyInMatchException();
+        }
+
         scores.put(homeTeam, awayTeam);
 
         log.info("Game for {} and {} added successfully", homeTeam, awayTeam);
+    }
+
+    private boolean checkIfTeamHasStartedMatch(String team) {
+        return scores.containsKey(team) || scores.containsValue(team);
     }
 
     public void updateScore(String homeTeam, String awayTeam) {
